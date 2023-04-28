05f0cccd8e1ca102e818cd5e646cf69448b8ba16
Hlukhanych <hlukhanych.stanislav@student.uzhnu.edu.ua>
2023-04-28 11:08:18 +0300

    new

---
+++ Лаб 21/Зав1/Зав1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Зав1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static double F(double x)
+        {
+            double sum = 0;
+            if (x > 3)
+            {
+                for (int i = 1; i < 18; i++)
+                {
+                    sum += Math.Sin(Math.Pow(x, i));
+                }
+            }
+            else
+            {
+                sum = 15;
+                for (int i = 1; i < 6; i++)
+                {
+                    sum += Math.Tan(Math.Pow(x, i));
+                }
+            }
+            return sum;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double u = Math.Max(F(a), F(b));
+            textBox3.Text = u.ToString();
+        }
+    }
+}
---
+++ Лаб 21/Зав2/Зав2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Зав2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            int m = Convert.ToInt32(textBox2.Text);
+            double x = Convert.ToDouble(textBox3.Text);
+            double sum = Math.Cos(2 + x/n) + Math.Sin(2 / 5);
+            int step = 4;
+            double znam = n * (n - 1);
+            double mnozh = n - 2;
+            for (int i = 1; i < n; i++)
+            {             
+                sum += Math.Cos(2 + Math.Pow(x, step) / znam);
+                znam *= mnozh;
+                mnozh -= 1;
+                step += step;
+            }
+            for (int i = 2; i < m; i++)
+            {
+                sum += -1 * Math.Sin(Math.Pow(2, i) / 5);
+            }
+            textBox4.Text = sum.ToString();
+        }
+    }
+}
